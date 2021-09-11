b6db7a39f79e87708c26721871ff92d5fff5fa76
jose ramon <rabohopo@gmail.com>
2021-09-12 00:09:28 +0200

    Web updated

--- routes/places.js
+++ routes/places.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 
+const { findById } = require("./../models/place.model");
 const Place = require("./../models/place.model");
 
 router.get("/", (req, res) => {
@@ -31,14 +32,33 @@ router.get("/list", (req, res) => {
 
 router.get('/list/:id',(req,res)=>{
     const {id} = req.params
-  console.log('eeeeeeeeeeeeeeeeeeeesto es req params', req.params)
-    Place
-        .findById(id)
-        .then(response => {
-          console.log('estoy vivo')
-          res.render('places/edit-place', {response})})
-        .catch(err => console.log('Esto es un error de list edit',err))
-   })
+        console.log('eeeeeeeeeeeeeeeeeeeesto es req params', req.params)
+            Place
+                .findById(id)
+                .then(response => {
+                  console.log('estoy vivo',response)
+                  res.render('places/edit-place', {response})})
+                .catch(err => console.log('Esto es un error de list edit',err))
+})
+
+router.post('/list/:id',(req,res)=>{
+  console.log(req.body)
+  console.log(req.params)
+
+const {id} = req.params
+const {name,type, lat, lng} = req.body
+
+const location = {type: 'Point', coordinates:[lat,lng]}
+
+Place
+    .findByIdAndUpdate(id,{name,type,location},{new:true})
+    .then(placeUpdated => {
+      res.redirect('/places')
+      console.log('the new info about the place is', placeUpdated)})
+    .catch(err => console.log('esto es un error de actualizacion', err))
+
+
+})
 
 router.get('/delete/:id',(req,res)=>{
     const {id} = req.params
@@ -52,5 +72,15 @@ router.get('/delete/:id',(req,res)=>{
      .catch(err => console.log('Hubo un error:', err))
 
 })
+
+
+// router.get('/edit/:id',(req,res)=> {
+  
+//   const {id} = req.params
+//   Place
+//   .findById(id)
+//   .then(placeFinded => res.render())
+
+// })
 
 module.exports = router;
